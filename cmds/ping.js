335baa919e258a678f2c6efecc442ec921e0ff61
Ashwin-A-K <ashwinkumar3098@gmail.com>
2019-06-05 02:17:46 -0700

    discordbot up

---
+++ cmds/ping.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+  var ping = await client.ping;
+  return message.channel.send(
+    new Discord.RichEmbed()
+      .setDescription(`**${client.user.username} Ping: ${Math.floor(ping)}ms**`)
+      .setColor("#00FF00")
+  );
+};
+
+module.exports.help = {
+  name: "ping",
+  descr: "Shows the current ping of the bot server."
+};
